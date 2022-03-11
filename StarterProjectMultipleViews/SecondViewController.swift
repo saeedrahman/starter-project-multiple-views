7a9fe5b8c308c3224f32f202accb0ceaef5fd311
Saeed Rahman <saeedrahman@users.noreply.github.com>
2022-03-11 11:57:44 +0800

    Initial commit
    - project setup with two view controllers and classes
    - added pragma MARK statements for organisation to each view controller class
    - added .gitignore

---
+++ StarterProjectMultipleViews/SecondViewController.swift
@@ -0,0 +1,48 @@
+/*
+ 
+ SecondViewController.swift
+ 
+ This file will contain the code for the second viewcontroller.
+ Please ensure that your code is organised and is easy to read.
+ This means that you will need to both structure your code correctly,
+ in addition to using the correct syntax for Swift.
+ 
+ Unless you are told otherwise, ensure that you are using the
+ camelCase syntax. For example, outputLabel and firstName are good
+ examples of using the camelCase syntax.
+ 
+ Within each class, you can see clearly identified sections denoted by
+ MARK statements. These MARK statements allow you to structure and organise
+ your code.
+ 
+ - @IBOutlets should be listed under the MARK section on IBOutlets
+ - Variables and constants listed under the MARK section Variables and Constants
+ - Functions (including @IBActions) listed under the section on IBActions and Functions.
+ 
+ As you develop each view controller class with Swift code, please include
+ detailed comments to both demonstrate understanding, and which serve you as
+ a reminder as to what your code actually does.
+ 
+ */
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    
+    
+    
+    // MARK: - Variables and Constants
+    
+    
+    
+    // MARK: - IBActions and Functions
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
